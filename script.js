49f2af695033255fbddebe3f5747e7a0baa1763c
HifumiD <124528123+HifumiD@users.noreply.github.com>
2023-07-08 14:38:25 +0200

    Added search bar, save button.

--- script.js
+++ script.js
@@ -20,6 +20,10 @@ fetch(`https://ddragon.leagueoflegends.com/cdn/13.13.1/data/en_US/champion.json`
       imageElement.id = championName; // Set a unique id for each image
 
       championImagesContainer.appendChild(imageElement);
+
+      // Add drag event listeners
+      imageElement.addEventListener('dragstart', dragStart);
+      imageElement.addEventListener('dragend', dragEnd);
     });
 
     const championImages = document.querySelectorAll('.champion-image');
@@ -35,7 +39,24 @@ fetch(`https://ddragon.leagueoflegends.com/cdn/13.13.1/data/en_US/champion.json`
       dragArea.addEventListener('dragenter', dragEnter);
       dragArea.addEventListener('dragleave', dragLeave);
       dragArea.addEventListener('drop', dragDrop);
+
+      // Add click event listener to delete the image
+      dragArea.addEventListener('click', deleteImage);
     });
+
+    const searchBar = document.getElementById('searchBar');
+    searchBar.addEventListener('input', handleSearch);
+
+    // Add event listener to the "Save" button
+    const saveButton = document.getElementById('saveButton');
+    saveButton.addEventListener('click', saveImageLocations);
+
+    // Load image locations from local storage
+    const savedImageLocations = localStorage.getItem('imageLocations');
+    if (savedImageLocations) {
+      imageLocations = JSON.parse(savedImageLocations);
+      restoreImageLocations();
+    }
   })
   .catch(error => {
     console.error('Error:', error);
@@ -66,10 +87,76 @@ function dragDrop(event) {
   event.preventDefault();
   const draggedImageId = event.dataTransfer.getData('text/plain');
   const draggedImage = document.getElementById(draggedImageId);
-  this.appendChild(draggedImage);
+
+  // Clone the dragged image
+  const clonedImage = draggedImage.cloneNode(true);
+
+  // Append the cloned image to the draggable area
+  this.appendChild(clonedImage);
+
   this.classList.remove('drag-over');
 }
 
+function deleteImage(event) {
+  if (event.target.classList.contains('champion-image')) {
+    // Remove the clicked image from the DOM
+    event.target.remove();
+  }
+}
+
+function handleSearch(event) {
+  const searchTerm = event.target.value.toLowerCase();
+  filterChampionImages(searchTerm);
+}
+
+function filterChampionImages(searchTerm) {
+  const championImages = document.querySelectorAll('.champion-image');
+
+  championImages.forEach(championImage => {
+    const championName = championImage.alt.toLowerCase();
+
+    if (championName.includes(searchTerm)) {
+      championImage.classList.remove('fade-out');
+    } else {
+      championImage.classList.add('fade-out');
+    }
+  });
+}
+
+function saveImageLocations() {
+  const dragAreas = document.querySelectorAll('.drag-area');
+
+  dragAreas.forEach(dragArea => {
+    const dragAreaId = dragArea.id;
+    const imageElements = dragArea.querySelectorAll('.champion-image');
+
+    const imageIds = Array.from(imageElements).map(image => image.id);
+    imageLocations[dragAreaId] = imageIds;
+  });
+
+  // Save image locations to local storage
+  localStorage.setItem('imageLocations', JSON.stringify(imageLocations));
+}
+
+function restoreImageLocations() {
+  const dragAreas = document.querySelectorAll('.drag-area');
+
+  dragAreas.forEach(dragArea => {
+    const dragAreaId = dragArea.id;
+    const imageIds = imageLocations[dragAreaId];
+
+    if (imageIds && imageIds.length > 0) {
+      const championImagesContainer = document.getElementById('championImages');
+
+      imageIds.forEach(imageId => {
+        const imageElement = championImagesContainer.querySelector(`#${imageId}`);
+        if (imageElement) {
+          dragArea.appendChild(imageElement);
+        }
+      });
+    }
+  });
+}
 
 
 
